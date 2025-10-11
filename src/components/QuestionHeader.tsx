3a21f72bb845941ebe1c3ac616ec39d6484f8f3e
ErickDevPy <erickdiass066@gmail.com>
2025-10-11 06:40:57 -0300

    style: add question and question-counter classes for the component Question Header.

--- src/components/QuestionHeader.tsx
+++ src/components/QuestionHeader.tsx
@@ -5,9 +5,13 @@ interface QuestionHeaderProps {
 
 export function QuestionHeader({number, question}: QuestionHeaderProps) {
     return (
-        <div >
-            <h2>Question {number}</h2>
-            <p>{question}</p>
+        <div className="bg-white p-6 rounded-lg shadow-lg mb-6 border border-gray-100">
+            <h2 className="question-counter">
+                Question {number}
+            </h2>
+            <p className="question">
+                {question}
+            </p>
         </div>
     );
 }

859abb86cd16d5f43b4519d33c45b590cf5f94a9
ErickDevPy <erickdiass066@gmail.com>
2025-10-14 20:40:51 -0300

    feat: add Icons and classes to ExercisesPageHeader

--- src/components/ExercisesPageHeader.tsx
+++ src/components/ExercisesPageHeader.tsx
@@ -1,9 +1,13 @@
+import { ArrowBigLeft } from 'lucide-react';
+
 export function ExercisesPageHeader() {
     return (
         <div className="exercises-header">
             <div></div>
-            <span className="question-title">Exercises</span>
-            <button>◀</button>
+            <span className="header-title">Exercises</span>
+            <button>
+                <ArrowBigLeft size={36} className='left-icon'/>
+            </button>
         </div>
     )
 }

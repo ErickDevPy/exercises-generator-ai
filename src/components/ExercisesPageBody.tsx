3c4120063ff44ccaf1f75d0122c7047b9bc431f0
ErickDevPy <erickdiass066@gmail.com>
2025-10-15 00:24:15 -0300

    feat: add ExercisesPageBody empty questions if

--- src/components/ExercisesPageBody.tsx
+++ src/components/ExercisesPageBody.tsx
@@ -1,10 +1,23 @@
 import type { Exercises } from "~/types";
 import { QuestionsList } from "./QuestionsList";
+import { FileQuestion } from 'lucide-react'; 
 
 export function ExercisesPageBody({questions}: {questions: Exercises}) {
+    const hasQuestions = questions.length > 0;
+
     return (
         <div className="flex flex-col h-screen items-center hide-scrollbar overflow-y-scroll p-30">
-            {questions.length > 0 && <QuestionsList exercises={questions} />}
+            {hasQuestions ? (
+                <QuestionsList exercises={questions} />
+            ) : (
+                <div className="exercises-empty-container">
+                    <FileQuestion 
+                        className="exercises-empty-icon" 
+                    />
+                    <p className="text-2xl font-medium">No exercises found</p>
+                    <p className="text-xl">Please try generating new exercises</p>
+                </div>
+            )}
         </div>
     )
 }
